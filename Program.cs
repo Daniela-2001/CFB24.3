68e677f2348ed7e70501cfd38beac612eec3d10c
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-29 10:38:16 +0000

    CFB24.3

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace aula24._3
+{
+    internal class Program
+    {
+        static void Main(){
+            cfb();
+        }
+
+        static void cfb(){
+            Console.WriteLine("CFB Cursos");
+            Console.WriteLine("Cursos de C#");
+            Console.WriteLine("youtube.com/cfbcursos");
+
+        }
+    }
+}
